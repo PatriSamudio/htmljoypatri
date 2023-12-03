1a43eedbf007c63273fbdb26011ee038b233c46a
patricia <patty_samudio@hotmail.com>
2023-12-03 19:58:48 +0100

    favicon y lazy loading

--- index.js
+++ index.js
@@ -1,21 +1,8 @@
-/*document.addEventListener("DOMContentLoaded", function () {
-  var gifs = document.querySelectorAll(".gif");
+document.addEventListener("DOMContentLoaded", function() {
+  var gif = document.getElementsByClassName("gif");
 
-  gifs.forEach(function (gif) {
-    gif.addEventListener("load", function () {
-      playGifOnce(gif);
-    });
-  });
-
-  function playGifOnce(gif) {
-    var animationDuration = 2000; // Ajusta el tiempo en milisegundos según la duración de tu animación
-
-    setTimeout(function () {
-      gif.style.animationName = "no-repeat";
-      gif.style.animationDuration = "2s";
-      gif.style.animationFillMode = "forwards";
-      gif.style.animationPlayState = "paused"; // Pausa la animación después de la duración especificada
-    }, animationDuration);
-  }
+  gif.onload = function() {
+      // Cuando la imagen se ha cargado completamente
+      gif.classList.add("activarAnimacion");
+  };
 });
-*/
